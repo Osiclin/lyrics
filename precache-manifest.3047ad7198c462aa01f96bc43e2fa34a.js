453ac0adef79be90cec44d9791a9ad713514fbb6
Osiclin <67206380+Osiclin@users.noreply.github.com>
2020-09-19 08:28:13 +0100

    Updates

---
+++ precache-manifest.3047ad7198c462aa01f96bc43e2fa34a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1b283b296f7f55c79b33c5b4221d867",
+    "url": "/lyrics/index.html"
+  },
+  {
+    "revision": "f66d5830a46fe71e8a84",
+    "url": "/lyrics/static/css/main.975fdd6e.chunk.css"
+  },
+  {
+    "revision": "9312fe2dc75c3d73e2ca",
+    "url": "/lyrics/static/js/2.ddf64ec9.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/lyrics/static/js/2.ddf64ec9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f66d5830a46fe71e8a84",
+    "url": "/lyrics/static/js/main.63eafe78.chunk.js"
+  },
+  {
+    "revision": "63ac4f358dd881491e2c",
+    "url": "/lyrics/static/js/runtime-main.cfabec72.js"
+  },
+  {
+    "revision": "eea681d9a9e3cc085e0ceb881672a21f",
+    "url": "/lyrics/static/media/bg1.eea681d9.jpg"
+  }
+]);
